a8a115788cf031f8d45d896a2848abf075c9cc22
hilal <aysehilal88@gmail.com>
2026-06-05 15:46:47 -0400

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        var seen = new Dictionary<int, int>();  // value -> index
+
+        for (int i = 0; i < nums.Length; i++) {
+            int need = target - nums[i];
+
+            if (seen.TryGetValue(need, out int j)) {
+                return [j,i];
+            }
+
+            seen[nums[i]] = i;
+        }
+
+        return [-1,-1];
+    }
+}
